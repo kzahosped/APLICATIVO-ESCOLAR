2c7fac4fd822721904116d79f7afe75b4fd554cb
kzahosped <guagency29084@ai9.premium3.io.vn>
2025-11-26 21:05:52 -0300

    feat: setup firebase hosting and automated android apk build

---
+++ android/app/src/main/java/com/sdc/gestao/MainActivity.java
@@ -0,0 +1,5 @@
+package com.sdc.gestao;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.sdc.gestao',
+  appName: 'SDC Gestão',
+  webDir: 'dist'
+};
+
+export default config;
